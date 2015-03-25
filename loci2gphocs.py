f63d1de57517fed3bbb58019e27167f10a66ad19
deren <daeaton.chicago@gmail.com>
2015-03-25 15:02:15 -0400

    fix for gphocs paired data remove nnnn separator

--- loci2gphocs.py
+++ loci2gphocs.py
@@ -31,29 +31,30 @@ def make(WORK, outname):
     # Then print the data for each sample in this format:
     # <individual_name> <sequence>
     for i, loc in enumerate(loci):
-        # Get the length of the sequence read at this locus so we can print the
-        # header. First sequence is on the second line (first is snp line)
-        # then you take the 2nd element with [1]
-        first_sequence = loc.strip().split("\n")[1]
-        sequence_length = len(first_sequence.strip().split()[1])
-    
         # Separate out each sequence within the loc block. 'sequences'
         # will now be a list strings containing name/sequence pairs.
         # We select each line in the locus string that starts with ">"
-        sequences = [line for line in loc.strip().split("\n") if line[0]==">"]
+        names = [line.split()[0] for line in loc.strip().split("\n") if ">" in line]
+        sequences = [line.split()[-1] for line in loc.strip().split("\n") if ">" in line] 
 
+        # Strips off 'nnnn' separator for paired data
+        # replaces '-' with 'N'
+        editsequences = [seq.replace("n","").replace('-','N') for seq in sequences]
+        sequence_length = len(editsequences[0])
+
+        # get length of longest name and add 4 spaces
+        longname = max(map(len,names))+4
+        
         # Print out the header for this locus
         print >>outfile, 'locus'+ str(i), len(sequences), str( sequence_length )
     
         # Iterate through each sequence read at this locus and write it to the file.
-        for sequence in sequences:
+        for name,sequence in zip(names,sequences):
             # Clean up the sequence data to make gphocs happy. Only accepts UPPER
             # case chars for bases, and only accepts 'N' for missing data. Also,
             # the .loci format prepends a '>' on to the individual names, so we have
             # to clean this up which is what the [1:] is doing.
-            sequence = sequence[1:].upper().replace( '-', 'N' )
-    
-            print >>outfile, sequence
+            print >>outfile, name[1:]+" "*(longname-len(name))+sequence
 
 if __name__ == "__main__":
     make(WORK, outfile)
