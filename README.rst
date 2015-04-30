16d6afd326a0593807e1959e3e82c2ba77842561
Augustine Dunn <wadunn83@gmail.com>
2015-04-30 14:47:15 -0400

    begining to convert to pip-style install

---
+++ README.rst
@@ -0,0 +1,32 @@
+pyrad
+=====
+
+
+Assembly and analysis of RADseq data sets
+
+Tutorials
+---------
+
+Detailed information and a number of example tutorials are 
+available `here <http://www.dereneaton.com/software/pyrad/>`_.    
+
+
+Downloads
+---------
+
+Stable release versions can be downloaded `here <https://github.com/dereneaton/pyrad/releases>`_, or you can clone the development version using git. 
+
+
+Python Requirements
+^^^^^^^^^^^^^^^^^^^
+
+ * numpy
+ * scipy
+
+Licence
+-------
+
+Authors
+-------
+
+`pyrad` was written by `Deren Eaton <deren.eaton@yale.edu>`_.
---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+requirements = [
+    'numpy',
+    'scipy',
+    ]
+
+setuptools.setup(
+    name="pyrad",
+    version="3.0.5",
+    url="https://github.com/dereneaton/pyrad",
+
+    author="Deren Eaton",
+    author_email="deren.eaton@yale.edu",
+
+    description="Assembly and analysis of RADseq data sets",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[requirements],
+
+
+    entry_points={
+            'console_scripts': [
+                'pyRAD = pyRAD:main',
+            ],
+    },
+
+    license='GPL',
+
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
