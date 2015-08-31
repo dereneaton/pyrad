2be41fee30a80b07c48e07b530f2ed2ab8571b11
deren <daeaton.chicago@gmail.com>
2015-08-31 17:38:03 -0400

    faster phylip output

--- pyrad/alignable.py
+++ pyrad/alignable.py
@@ -838,8 +838,7 @@ def main(params, infile, taxadict, minhits, version, quiet):
         os.makedirs(params["work"]+'stats')
 
     ## read names from file
-        
-    temp = gzip.open(infile, 'r').readlines()
+    temp = iter(gzip.open(infile, 'rb'))
     names = set(["_".join(i.split(">")[1].split("_")[:-2]) \
                 for i in temp if ">" in i])
 
