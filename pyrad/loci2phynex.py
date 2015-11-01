943dd9e9bf8f7969b38b13b3ec1d4df248adfaf1
deren <daeaton.chicago@gmail.com>
2015-11-01 14:25:38 -0500

    added partitioning info for phy nex outputs

--- pyrad/loci2phynex.py
+++ pyrad/loci2phynex.py
@@ -8,7 +8,8 @@ import glob
 
 def update(idict, count, WORK, outname):
     """ updates dictionary with the next .5M reads 
-    from the super long string phylip file """
+    from the super long string phylip file. Makes
+    for faster reading. """
 
     data = iter(open(WORK+"outfiles/"+outname+".phy"))
     ntax, nchar = data.next().strip().split()
@@ -24,19 +25,26 @@ def update(idict, count, WORK, outname):
     
 
 
-def makephy(WORK, outname, names, longname, formats):
+def makephy(WORK, outname, names, longname):
+    """ builds phy output. If large files writes 50000 loci 
+        at a time to tmp files and rebuilds at the end"""
+
     " order names "
     names = list(names)
     names.sort()
     
     " read in loci file "
-    locifile = open(WORK+"outfiles/"+outname+".loci", 'rb')
+    locus = iter(open(WORK+"outfiles/"+outname+".loci", 'rb'))
 
     " dict for saving the full matrix "
     fdict = {name:[] for name in names}
 
+    " list for saving locus number and locus range for partitions "
+    partitions = []
+    loc_number = 1
+    initial_pos = 1
+
     " remove empty column sites and append edited seqs to dict F "
-    locus = iter(locifile)
     done = 0
     nloci = 0
     nbases = 0
@@ -53,7 +61,6 @@ def makephy(WORK, outname, names, longname, formats):
                 break
             if "//" in samp:
                 nloci += 1
-                #print arrayed
                 break
             else:
                 try:
@@ -69,10 +76,16 @@ def makephy(WORK, outname, names, longname, formats):
         ## create mask for columns that are empty or 
         ## that are paired-end separators (compatible w/ pyrad v2 and v3)
         #mask = [i for i in range(len(arrayed.T)) if np.any([
+        ## still surely a better way to vectorize this...
         mask = [i for i in arrayed.T if any([j not in list("-Nn") for j in i])]
         masked = np.dstack(mask)[0]
-        #not \
-        #        np.all([j in list("N-n") for j in arrayed.T[i]])]
+
+        ## partition information
+        loc_name = "p"+str(nloci)
+        loc_range = str(initial_pos) + "-" +\
+                    str(len(masked[0]) + initial_pos -1)
+        initial_pos += len(masked[0])
+        partitions.append(loc_name+"="+loc_range)
 
         ## uncomment to print block info (used to partition by locus)
         #blockend += minray
@@ -121,60 +134,72 @@ def makephy(WORK, outname, names, longname, formats):
                     superout.write(tmpin.read())
             superout.write("\n")
     superout.close()
+    raxml_part_out = open(WORK+"outfiles/"+outname+".phy.partitions", 'w')
+    for partition in partitions:
+        print >>raxml_part_out, "DNA, %s" % (partition)
+    raxml_part_out.close()
+
+    return partitions
 
 
-
-def makenex(WORK, outname, names, longname, formats):
+def makenex(WORK, outname, names, longname, partitions):
     """ PRINT NEXUS """
-    if 1:
-        " make nexus output "
-        data   = iter(open(WORK+"outfiles/"+outname+".phy"))
-        nexout = open(WORK+"outfiles/"+outname+".nex", 'wb')
 
-        ntax, nchar = data.next().strip().split(" ")
+    " make nexus output "
+    data   = iter(open(WORK+"outfiles/"+outname+".phy"))
+    nexout = open(WORK+"outfiles/"+outname+".nex", 'wb')
 
-        print >>nexout, "#NEXUS"
-        print >>nexout, "BEGIN DATA;"
-        print >>nexout, "  DIMENSIONS NTAX=%s NCHAR=%s;" % (ntax,nchar)
-        print >>nexout, "  FORMAT DATATYPE=DNA MISSING=N GAP=- INTERLEAVE=YES;"
-        print >>nexout, "  MATRIX"
+    ntax, nchar = data.next().strip().split(" ")
 
-        idict = {}
+    print >>nexout, "#NEXUS"
+    print >>nexout, "BEGIN DATA;"
+    print >>nexout, "  DIMENSIONS NTAX=%s NCHAR=%s;" % (ntax,nchar)
+    print >>nexout, "  FORMAT DATATYPE=DNA MISSING=N GAP=- INTERLEAVE=YES;"
+    print >>nexout, "  MATRIX"
 
-        ## read in max 1M bp at a time
-        for line in data:
-            tax, seq = line.strip().split()
-            idict[tax] = seq[0:100000]
-        del line
+    idict = {}
 
-        nameorder = idict.keys()
-        nameorder.sort()
+    ## read in max 1M bp at a time
+    for line in data:
+        tax, seq = line.strip().split()
+        idict[tax] = seq[0:100000]
+    del line
 
-        n=0
-        tempn=0
-        sz = 100
-        while n < len(seq):
-            for tax in nameorder:
-                print >>nexout, "  "+tax+" "*\
-                                 ((longname-len(tax))+3)+\
-                                 idict[tax][tempn:tempn+sz]
-            n += sz
-            tempn += sz
-            print >>nexout, ""
+    nameorder = idict.keys()
+    nameorder.sort()
 
-            if not n % 100000:
-                #print idict[tax][tempn:tempn+sz]
-                idict = update(idict, n, WORK, outname)
-                tempn -= 100000
+    n=0
+    tempn=0
+    sz = 100
+    while n < len(seq):
+        for tax in nameorder:
+            print >>nexout, "  "+tax+" "*\
+                             ((longname-len(tax))+3)+\
+                             idict[tax][tempn:tempn+sz]
+        n += sz
+        tempn += sz
+        print >>nexout, ""
+
+        if not n % 100000:
+            #print idict[tax][tempn:tempn+sz]
+            idict = update(idict, n, WORK, outname)
+            tempn -= 100000
             
-        print >>nexout, ';'
-        print >>nexout, 'END;'
-        nexout.close()
+    print >>nexout, ';'
+    print >>nexout, 'END;'
+    
+    ### partitions info
+    print >>nexout, "BEGIN SETS;"
+    for partition in partitions:
+        print >>nexout, "  CHARSET %s;" % (partition)
+    print >>nexout, "END;"
+
+    nexout.close()
         
 
 def make(WORK, outfile, names, longname, formats):
-    makephy(WORK, outfile, names, longname, formats)
-    makenex(WORK, outfile, names, longname, formats)
+    partitions = makephy(WORK, outfile, names, longname)
+    makenex(WORK, outfile, names, longname, partitions)
     
 
 if __name__ == "__main__":
